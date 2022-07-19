60a42e47952976764a3a6319cb97ee44d7a827cb
havenquan <havenquan@qq.com>
2022-07-19 18:05:43 +0800

    avax -> eth

--- src/constant.ts
+++ src/constant.ts
@@ -5,20 +5,18 @@ export function token(env?: string) {
       CB: '0x',
       CN: '0x',
       CS: '0x',
-      FUN: '0x3c1c0Bd9bFD90254916C4cb5e7221Ce907b4D4b1',
+      FUN: '0x',
       FUNLP: '0x',
-      USDT: '0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7',
-      WAVAX: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
+      USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
     }
   } else {
     return {
-      CB: '0x20292b2D33A5632a44b3D00FeB30d687E112192F',
-      CN: '0x8FeB8ad9282b22fEE33F5da25f4aCAFe4BFAA925',
-      CS: '0x7E47837BBCB61D9675C66e6e64c6b307E5687F3A',
-      FUN: '0x18b2978D7E4EAc4A00c66Ab00d68bA7E77141A79',
+      CB: '0x',
+      CN: '0x',
+      CS: '0x',
+      FUN: '0x',
       FUNLP: '0x',
-      USDT: '0xb3e0eFcD78829B319BE3D089D88e88D6Cf092B13',
-      WAVAX: '0xd00ae08403B9bbb9124bB305C09058E32C39A48c',
+      USDT: '0x',
     };
   }
 };
@@ -28,16 +26,14 @@ export function contract(env?: string) {
   if (environment === 'production') {
     return {
       Market: '0x',
-      JoeRouter: '0x60aE616a2155Ee3d9A68541Ba4544862310933d4',
       USDTDeposit: '0x',
       USDTWithdraw: '0x',
     };
   } else {
     return {
-      Market: '0xFfc78d708c478a75f1a0d0cd6889940eC141A51f',
-      JoeRouter: '0x',
-      USDTDeposit: '0x40df95Bea05a5ec9Af4dD7532ebab88eB37DA136',
-      USDTWithdraw: '0xa526a51108076A8533747877Dbe73f73270c7954',
+      Market: '0x',
+      USDTDeposit: '0x',
+      USDTWithdraw: '0x',
     };
   }
 };
@@ -46,15 +42,15 @@ export function network(env?: string) {
   const environment = env || process.env.NODE_ENV;
   if (environment === 'production') {
     return {
-      chainId: '0xa86a',
-      chainName: 'Avalanche-Mainnet',
-      rpcUrls: ['https://api.avax.network/ext/bc/C/rpc']
+      chainId: '0x1',
+      chainName: 'Ethereum-Mainnet',
+      rpcUrls: ['https://mainnet.infura.io/v3/']
     };
   } else {
     return {
-      chainId: '0xa869',
-      chainName: 'Avalanche-Testnet',
-      rpcUrls: ['https://api.avax-test.network/ext/bc/C/rpc']
+      chainId: '0x4',
+      chainName: 'Rinkeby-Testnet',
+      rpcUrls: ['https://rinkeby.infura.io/v3/']
     };
   }
 };
