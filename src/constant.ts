d1baf23c7ede506080c3787d70c32de545859653
havenquan <havenquan@qq.com>
2022-07-20 16:34:41 +0800

    10

--- src/constant.ts
+++ src/constant.ts
@@ -11,12 +11,12 @@ export function token(env?: string) {
     }
   } else {
     return {
-      CB: '0x',
-      CN: '0x',
+      CB: '0xD44834B07A61D032Fbef14d5Cc0C4585159dD753',
+      CN: '0x2432e8dD4202C09aF83F7f20b1f09378349179cb',
       CS: '0x',
-      FUN: '0x',
+      FUN: '0x7FDc9543447930cFCCC7f82976547C3DcD64db81',
       FUNLP: '0x',
-      USDT: '0x',
+      USDT: '0x4ED9358320f140D1a1a22EBfc9B53ED018C22e49',
     };
   }
 };
@@ -31,9 +31,9 @@ export function contract(env?: string) {
     };
   } else {
     return {
-      Market: '0x',
-      USDTDeposit: '0x',
-      USDTWithdraw: '0x',
+      Market: '0x006C514e4e43926639Baf36Df27fB784D9665935',
+      USDTDeposit: '0x584A6ab917fEca409bAdA89d599b8D642281D3B6',
+      USDTWithdraw: '0x84cbaBaD607e9867B74924f6A5C8E5f73bEb70A2',
     };
   }
 };
